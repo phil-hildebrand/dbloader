b5eeb0ca402fb03187b7cf1b4244143e5e682546
Phil H <phil.hildebrand@moz.com>
2018-01-17 10:43:34 -0800

    Verify both python 2.7 & 3.x work

--- bootstrap.sh
+++ bootstrap.sh
@@ -23,4 +23,8 @@ cd /vagrant
 pip install -r requirements.txt
 python setup.py install
 pytest -v tests
+. /vagrant/venv/bin/activate
+python setup.py install
+pytest -v tests
+pip install -r requirements.txt
 echo ". /vagrant/p3env/bin/activate; cd /vagrant" >> ~vagrant/.bashrc
