070153959a4c6f82aff322de8cd8e7b798ef53ed
Phil H <phil.hildebrand@moz.com>
2018-01-29 17:35:46 -0800

    Add support and tests for rethink version 2.2

--- dbloader/rethink_example.py
+++ dbloader/rethink_example.py
@@ -1,40 +1,37 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+from gevent import monkey
+monkey.patch_all()
+import json
+import os
+import sys
+import logging
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), './dbloader')))
 import dbloader as dbl
 
+logger = logging.getLogger(__name__)
+
 dbl.setup_logs('./dbloader.log', True)
-ldr = dbl.rl.RethinkLoader()
-ldr.host = 'localhost'
-ldr.port = 28015
-ldr.inserts = 50
-ldr.deletes = 2
-ldr.updates = 2
-ldr.selects = 2
-ldr.concurrency = 5
-ldr.itterations = 5
-ldr.databases = ['rt_db_1', 'rt_db_2']
-ldr.tables = ['ltbl_1', 'ltbl_2']
-ldr.insert_some()
-# ldr.conn = ldr.get_connection(ldr.host, ldr.port)
-# if ldr.create_if_not_exists(ldr.conn):
-#     inserted = ldr.insert_some()
-#     deleted = ldr.delete_some()
-#     dbl.logger.info("Inserted %d records in %4.3f sec for an avg insert time of %4.2f" % (len(inserted),
-#                                                                                       sum(inserted),
-#                                                                                       sum(inserted) / len(inserted)))
-#     dbl.logger.info("Deleted %d records in %4.3f sec for an avg delete time of %4.2f" % ( len(deleted),
-#                                                                                       sum(deleted),
-#                                                                                       sum(deleted) / len(deleted)))
-#     inserted, deleted, updated, selected = ldr.load_run()
-#     dbl.logger.info("Run: Inserted %d in %4.3f sec for an avg insert time of %4.2f" % (len(inserted),
-#                                                                                       sum(inserted),
-#                                                                                       sum(inserted) / len(inserted)))
-#     dbl.logger.info("Run: Deleted %d in %4.3f sec for an avg delete time of %4.2f" % (len(deleted),
-#                                                                                       sum(deleted),
-#                                                                                       sum(deleted) / len(deleted)))
-#     dbl.logger.info("Run: Updated %d in %4.3f sec for an avg update time of %4.2f" % (len(updated),
-#                                                                                       sum(updated),
-#                                                                                       sum(updated) / len(updated)))
-#     dbl.logger.info("Run: Selected %d in %4.3f sec for an avg select time of %4.2f" % (len(selected),
-#                                                                                       sum(selected),
-#                                                                                       sum(selected) / len(selected)))
-# else:
-#    dbl.logger.warning("Could not verify databases/tables")
+options = dbl.load_config('./etc/load.yml')
+if not options:
+    exit(1)
+for server in options['server']:
+    node = server['name']
+    dbtype = server['type']
+    port = server['port']
+    user = server['user']
+    passwd = server['pass']
+    if dbtype == 'rethink':
+        ldr = dbl.rl.RethinkLoader()
+        ldr.host = node
+        ldr.custom = server['custom']
+        ldr.port = server['port']
+        ldr.databases = ['rt_db_1', 'rt_db_2']
+        ldr.tables = ['ltbl_1', 'ltbl_2']
+        ldr.inserts = server.get('inserts', 50)
+        ldr.deletes = server.get('deletes', 5)
+        ldr.updates = server.get('updates', 5)
+        ldr.selects = server.get('selects', 5)
+        ldr.concurrency = 5
+        ldr.itterations = 2
+        dbl.main(dbtype, ldr)
